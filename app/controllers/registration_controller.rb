301fab51a0455dbd3c80f543b9bd505fe4dc4e87
Oren Dobzinski <oren@desk.com>
2013-08-26 00:02:52 -0400

    lambda style

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationController < ApplicationController
   def create
-    @user = RegistersUser.call(params[:username])
+    @user = RegistersUser.(params[:username])
     render json: @user
   end
 end
