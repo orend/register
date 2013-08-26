138cb75df0ce824650ba3242ee3788bea3fefca0
Oren Dobzinski <oren@desk.com>
2013-08-25 23:56:54 -0400

    User fetche's parenthesis

--- app/services/registers_user.rb
+++ app/services/registers_user.rb
@@ -1,6 +1,6 @@
 class RegistersUser
   def register(username, options)
-    user_creator = options.fetch(:user_creator, User)
+    user_creator = options.fetch(:user_creator) { User }
     user_creator.find_or_create_by(username: username)
   end
 end
