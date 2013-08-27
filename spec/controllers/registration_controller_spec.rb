a3c37e9bcdc7be94e151a13e21b8187711b7b410
Oren Dobzinski <oren@desk.com>
2013-08-26 22:00:55 -0400

    change comment

--- spec/controllers/registration_controller_spec.rb
+++ spec/controllers/registration_controller_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
 
 describe RegistrationController, :type => :controller do
-  it 'craetes a new user' do
+  it 'creates a new user if username does not exist' do
     post :create, {username: 'username'}
     expect(assigns(:user).username).to eq('username')
   end
-
 end
