9615ca70581174d9827e6e6b5eb8ff428b18ceb7
Oren Dobzinski <oren@desk.com>
2013-08-26 23:03:14 -0400

    use rspec-mock, not mocha

--- spec/unit/services/registers_user_spec.rb
+++ spec/unit/services/registers_user_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 describe RegistersUser do
-  let(:user_creator) { mock('user_creator') }
+  let(:user_creator) { double('user_creator') }
   subject(:registers_user) { RegistersUser.new }
 
   it 'registers a new user' do
-    user_creator.expects(:find_or_create_by).with(username: 'username')
+  	expect(user_creator).to receive(:find_or_create_by).with(username: 'username')
     registers_user.(username: 'username', user_creator: user_creator)
   end
 
