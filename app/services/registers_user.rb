278782adf5fde9fe9f327bd4738470d3503d6285
Oren Dobzinski <oren@desk.com>
2013-08-30 01:46:57 -0400

    do more things with user

--- app/services/registers_user.rb
+++ app/services/registers_user.rb
@@ -2,6 +2,13 @@ class RegistersUser
   def call(params = {})
     username = params.fetch(:username)
     user_creator = params.fetch(:user_creator) { User }
-    user_creator.find_or_create_by(username: username)
+    user_creator.find_or_create_by(username: username).tap do |user|
+      do_more_things_with(user)
+    end
+  end
+
+  private
+
+  def do_more_things_with(user)
   end
 end
