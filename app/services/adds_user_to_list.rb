efaf52b51644894a093bb3e81009f03f570cac8d
Oren Dobzinski <oren@desk.com>
2013-09-03 01:07:34 -0400

    rename user_creator to creates_user

--- app/services/adds_user_to_list.rb
+++ app/services/adds_user_to_list.rb
@@ -1,9 +1,9 @@
 class AddsUserToList
   def self.call(username, email_list_name, params = {})
-    user_creator = params.fetch(:user_creator) { User }
+    creates_user = params.fetch(:creates_user) { User }
     notifies_user = params.fetch(:notifies_user) { NotifiesUser }
 
-    user_creator.find_or_create_by(username: username).tap do |user|
+    creates_user.find_or_create_by(username: username).tap do |user|
       notifies_user.(user, email_list_name)
       user.update_attributes(email_list_name: email_list_name)
     end
--- spec/unit/services/adds_user_to_list_spec.rb
+++ spec/unit/services/adds_user_to_list_spec.rb
@@ -2,18 +2,18 @@ require 'spec_unit_helper'
 require "./app/services/adds_user_to_list"
 
 describe AddsUserToList do
-  let(:user_creator) { double('user_creator') }
+  let(:creates_user) { double('creates_user') }
   let(:notifies_user) { double('notifies_user') }
   let(:user) { double('user') }
   subject(:adds_user_to_list) { AddsUserToList }
 
   it 'registers a new user' do
-  	expect(user_creator).to receive(:find_or_create_by)
+  	expect(creates_user).to receive(:find_or_create_by)
   			.with(username: 'username').and_return(user)
   	expect(notifies_user).to receive(:call).with(user, 'list_name')
   	expect(user).to receive(:update_attributes).with(email_list_name: 'list_name')
 
-    adds_user_to_list.('username', 'list_name', user_creator: user_creator,
+    adds_user_to_list.('username', 'list_name', creates_user: creates_user,
     	notifies_user: notifies_user)
   end
 
