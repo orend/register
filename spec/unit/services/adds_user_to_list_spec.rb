dd48478ae97a3fbce7942fefbd83b6e0f24d20a7
Oren Dobzinski <oren@desk.com>
2013-09-06 16:44:13 -0400

    formatting

--- spec/unit/services/adds_user_to_list_spec.rb
+++ spec/unit/services/adds_user_to_list_spec.rb
@@ -8,12 +8,10 @@ describe AddsUserToList do
   subject(:adds_user_to_list) { AddsUserToList }
 
   it 'registers a new user' do
-    expect(creates_user).to receive(:find_or_create_by)
-        .with(username: 'username').and_return(user)
+    expect(creates_user).to receive(:find_or_create_by).with(username: 'username').and_return(user)
     expect(notifies_user).to receive(:call).with(user, 'list_name')
     expect(user).to receive(:update_attributes).with(email_list_name: 'list_name')
 
-    adds_user_to_list.('username', 'list_name', creates_user: creates_user,
-      notifies_user: notifies_user)
+    adds_user_to_list.('username', 'list_name', creates_user: creates_user, notifies_user: notifies_user)
   end
 end
