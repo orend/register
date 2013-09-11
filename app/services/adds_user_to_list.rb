9d3b3763bcf24e87b1b3633b9bb295d62f1c3a82
Oren Dobzinski <oren@desk.com>
2013-09-11 00:04:04 -0400

    don't use temp vars for 2 options

--- app/services/adds_user_to_list.rb
+++ app/services/adds_user_to_list.rb
@@ -2,12 +2,10 @@ class AddsUserToList
   def self.call(params)
     creates_user = params.fetch(:creates_user) { User }
     notifies_user = params.fetch(:notifies_user) { NotifiesUser }
-    username = params.fetch(:username)
-    email_list_name = params.fetch(:email_list_name)
 
-    creates_user.find_or_create_by(username: username).tap do |user|
-      notifies_user.(user, email_list_name)
-      user.update_attributes(email_list_name: email_list_name)
+    creates_user.find_or_create_by(username: params.fetch(:username)).tap do |user|
+      notifies_user.(user, params.fetch(:email_list_name))
+      user.update_attributes(email_list_name: params.fetch(:email_list_name))
     end
   end
 end
