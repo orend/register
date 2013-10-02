ad61a69fad9371dca5d63057a9c3c29e2369ad75
Oren Dobzinski <oren@desk.com>
2013-10-02 12:45:36 -0400

    a small fix to the tests

--- app/services/adds_user_to_list.rb
+++ app/services/adds_user_to_list.rb
@@ -1,6 +1,6 @@
 class AddsUserToList
-  def self.call(username:, email_list_name:,
-    finds_user: User, notifies_user: NotifiesUser)
+  def self.call(username:, email_list_name:, finds_user: User,
+                notifies_user: NotifiesUser)
 
     user = finds_user.find_by_username!(username)
     notifies_user.(user, email_list_name)
--- spec/unit/services/adds_user_to_list_slow_spec.rb
+++ spec/unit/services/adds_user_to_list_slow_spec.rb
@@ -7,7 +7,7 @@ describe AddsUserToList do
   let(:notifies_user) { double('notifies_user') }
   subject(:adds_user_to_list) { AddsUserToList }
 
-  it 'registers a new user' do
+  it 'registers a new user (slow version)' do
     expect(notifies_user).to receive(:call)
     adds_user_to_list.(username: 'username', email_list_name: 'list_name', notifies_user: notifies_user)
   end
