b5495a8606d2edb87afd3b5428a423f8377a6d4e
Oren Dobzinski <oren@desk.com>
2013-10-01 22:00:07 -0400

    use ruby 2.1 keyword argumets syntax

--- app/services/adds_user_to_list.rb
+++ app/services/adds_user_to_list.rb
@@ -1,11 +1,10 @@
 class AddsUserToList
-  def self.call(args)
-    finds_user = args.fetch(:finds_user) { User }
-    notifies_user = args.fetch(:notifies_user) { NotifiesUser }
+  def self.call(username:, email_list_name:,
+    finds_user: User, notifies_user: NotifiesUser)
 
-    user = finds_user.find_by_username!(args.fetch(:username))
-    notifies_user.(user, args.fetch(:email_list_name))
-    user.add_to_mailing_list(args.fetch(:email_list_name))
+    user = finds_user.find_by_username!(username)
+    notifies_user.(user, email_list_name)
+    user.add_to_mailing_list(email_list_name)
     user
   end
 end
